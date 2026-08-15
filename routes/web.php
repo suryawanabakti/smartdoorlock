1b5903ccac52122d1995c02ab3f906d8d4bf0424
surya <suryawanabakti18@gmail.com>
2026-08-15 13:01:11 +0800

    PUSH

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AbsensiController;
+use App\Http\Controllers\Api\DoorLockController;
 use App\Http\Controllers\CalendarController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\DosenController;
@@ -21,6 +22,8 @@ use App\Http\Controllers\RuanganController;
 use App\Http\Controllers\ScanerStatusController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
+
+Route::get('/ambilpostpin', [DoorLockController::class, 'index2']);
 
 Route::get('/', [LandingPageController::class, 'index'])->name('home');
 
